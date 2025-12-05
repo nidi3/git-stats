681fb38f9727e316806d4365d3ec9db7887910b7
Stefan Niederhauser <ghuder5@gmx.ch>
2025-12-05 12:27:02 +0100

    initial commit

--- git_stats.py
+++ git_stats.py
@@ -22,7 +22,7 @@ def find_commits(dir: str, after: str, before: str, period: str) -> dict[str, di
     options = ""
     if after: options = options + f"--after={after} "
     if before: options = options + f"--before={before} "
-    lines = os.popen(f'git --no-pager log --all --reverse --format="%ct %H" {options}').readlines()
+    lines = os.popen(f'git --no-pager log --no-merges --all --format="%ct %H" {options}').readlines()
     commits = {}
     current = 0
     for line in lines:
@@ -54,10 +54,19 @@ def find_text(commits: dict[str, dict], find: dict[str, str]):
                 commit = commits[parts[0]]
                 commit[name] = commit.get(name, 0) + int(parts[-1])
 
-        print(commits[batch[-1]]["date"])  # , end='\r')
+        print(commits[batch[0]]["date"])  # , end='\r')
 
 
-def draw_chart(commits: dict[str, dict], names):
+def find_files(commits: dict[str, dict]):
+    for id, commit in commits.items():
+        lines = os.popen(f"git ls-tree -r -l {id}").readlines()
+        lines_parts = [line.split(maxsplit=5) for line in lines]
+        commit["files"] = [{"size": int(parts[3]), "name": parts[4]} for parts in lines_parts]
+
+        print(commit["date"])  # , end='\r')
+
+
+def draw_find_chart(commits: dict[str, dict], names):
     fig = plt.gcf()
     fig.set_size_inches(10, 10)
     dates = [commit["date"] for commit in commits.values()]
@@ -68,19 +77,36 @@ def draw_chart(commits: dict[str, dict], names):
     plt.ylabel('Count')
     # plt.show()
     os.chdir(dirname(realpath(__file__)))
-    fig.savefig('counts.png')
+    fig.savefig('find-counts.png')
+    plt.close()
+
+
+def draw_files_chart(commits: dict[str, dict]):
+    fig = plt.gcf()
+    fig.set_size_inches(10, 10)
+    dates = [commit["date"] for commit in commits.values()]
+    plt.plot(dates, [len(commit["files"]) for commit in commits.values()])
+    plt.ylabel('Count')
+    # plt.show()
+    os.chdir(dirname(realpath(__file__)))
+    fig.savefig('file-counts.png')
     plt.close()
 
 
 def main():
     args = parse_args()
     commits = find_commits(args.directory, args.after, args.before, args.period)
-    find = {"Old simple": "extends Vue", "Old mixin": "extends mixins", "New": "<script setup"}
+    # find_files(commits)
+    # draw_files_chart(commits)
+
+    find = {"Old simple": "extends Vue", "Old mixin": "extends mixins", "New": "<script .*setup"}
+    # find={"link":"link_created","link2":"linkCreated"}
     find_text(commits, find)
     for commit in commits.values():
         commit["Old total"] = commit.get("Old simple", 0) + commit.get("Old mixin", 0)
         commit["Total"] = commit["Old total"] + commit.get("New", 0)
-    draw_chart(commits, ["Old simple", "Old mixin", "Old Total", "New", "Total"])
+    draw_find_chart(commits, ["Old simple", "Old mixin", "Old total", "New", "Total"])
+    # draw_chart(commits, ["link","link2"])
 
 
 main()
